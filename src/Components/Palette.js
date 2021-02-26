414d478987454621a6298e522f26535363e2d115
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-02-26 14:45:35 +0200

    Add Navbar

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import ColorBox from './ColorBox'
-import Slider from 'rc-slider'
+import Navbar from './Navbar'
 
 import '../styles/Palette.css'
-import 'rc-slider/assets/index.css'
+
 
 function Palette({ palette }) {
     const [level, setLevel] = useState(500)
@@ -18,15 +18,10 @@ function Palette({ palette }) {
 
     return (
         <div className="Palette">
-            <div className="slider">
-                <Slider
-                    defaultValue={level}
-                    min={100}
-                    max={900}
-                    step={100}
-                    onAfterChange={changeLevel}
-                />
-            </div>
+            <Navbar
+                level={level}
+                changeLevel={changeLevel}
+            />
             <div className="Palette-colors">
                 {colorBoxes}
             </div>
