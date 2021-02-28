37dbc769596865f01a26f602cab1019a72ce4732
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-02-28 10:31:24 +0200

    Move out  styles

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,50 +1,9 @@
 import React, { useState } from 'react'
 import ColorBox from './ColorBox'
 import Navbar from './Navbar'
+import PaletteFooter from './PaletteFooter'
 import { withStyles } from '@material-ui/styles'
-
-import '../styles/Palette.css'
-
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors: {
-        height: "90%"
-    },
-    goBack: {
-        width: "20%",
-        height: "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-        opacity: 1,
-        backgroundColor: "black",
-        "& a": {
-            color: "white",
-            width: "100px",
-            height: "30px",
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(255, 255, 255, 0.3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            textTransform: "uppercase",
-            border: "none",
-            textDecoration: "none"
-        }
-    }
-}
+import styles from '../styles/PaletteStyles'
 
 function Palette({ palette, classes }) {
     const [level, setLevel] = useState(500)
@@ -78,10 +37,7 @@ function Palette({ palette, classes }) {
             <div className={classes.colors}>
                 {colorBoxes}
             </div>
-            <footer className="Palette-footer">
-                {palette.paletteName}
-                <span className="emoji">{palette.emoji}</span>
-            </footer>
+            <PaletteFooter palette={palette} />
         </div>
     )
 }
---
+++ src/Components/PaletteFooter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { withStyles } from '@material-ui/styles'
+import styles from '../styles/PaletteFooterStyles'
+
+function PaletteFooter({ palette, classes }) {
+    return (
+        <footer className={classes.PaletteFooter}>
+            {palette.paletteName}
+            <span className={classes.emoji}>{palette.emoji}</span>
+        </footer>
+    )
+}
+
+export default withStyles(styles)(PaletteFooter)
